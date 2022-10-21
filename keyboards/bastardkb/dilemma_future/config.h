68c0fd2072caab7c17e5c2181c577d332ea21cf6
Charly Delay <charly@delay.ai>
2022-10-21 22:16:25 +0900

    dilemma_future: add missing key

--- keyboards/bastardkb/dilemma_future/config.h
+++ keyboards/bastardkb/dilemma_future/config.h
@@ -17,6 +17,37 @@
 
 #pragma once
 
+#include "config_common.h"
+
+/* Key matrix configuration. */
+
+#define MATRIX_ROWS 8 // Rows are doubled-up.
+#define MATRIX_COLS 5
+
+#define DIODE_DIRECTION ROW2COL
+
+// Set 0 if debouncing isn't needed.
+#define DEBOUNCE 5
+
+/* Pointing device configuration. */
+
+// Enable use of pointing device on slave split.
+#define SPLIT_POINTING_ENABLE
+
+// Pointing device is on the right split.
+#define POINTING_DEVICE_RIGHT
+
+// Limits the frequency that the sensor is polled for motion.
+#define POINTING_DEVICE_TASK_THROTTLE_MS 10
+
+// Adjust trackpad rotation.
+#define POINTING_DEVICE_ROTATION_90
+
+// Configure for the Cirque model used on the Dilemma.
+#define CIRQUE_PINNACLE_DIAMETER_MM 35
+#define CIRQUE_PINNACLE_CURVED_OVERLAY
+#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE // Circular scroll.
+
 /* Key matrix configuration. */
 #define MATRIX_ROW_PINS \
     { GP4, GP5, GP27, GP26 }
